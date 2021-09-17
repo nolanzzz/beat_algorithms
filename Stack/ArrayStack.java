ba71179294221ba491f26707e961e0185868d4ba
nolanzzz <nolanwork131@gmail.com>
2021-09-17 17:50:25 -0400

    clean type comments to md files for queue and stack

--- Stack/ArrayStack.java
+++ Stack/ArrayStack.java
@@ -1,25 +1,5 @@
 package DataStructures.Stack;
 
-/*
-
-Stack:
-
-- Implement undo feature
-- Build compilers
-- Evaluate expressions
-- Build navigation (e.g. forward/back)
-
-Last In First Out (LIFO)
-
-Build Stack with Array or LinkedList
-
-All in constant time - O(1):
-push
-pop
-peek
-isEmpty
-
- */
 public class ArrayStack {
 
     private int[] items;
